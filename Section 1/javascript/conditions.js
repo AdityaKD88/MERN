cc3f1f56ec549891a011488530c2fab2306ba1e3
AdityaKD88 <90971914+AdityaKD88@users.noreply.github.com>
2023-09-16 20:37:04 +0530

    js

---
+++ Section 1/javascript/conditions.js
@@ -0,0 +1,30 @@
+age = 36;
+
+if(age>=18){
+    console.log('Eligible for DL');
+
+    myvar = 'Nice';
+    let x = 'Good';
+    const pi = 3.14;
+
+    console.log(pi);
+
+    // pi=5.14;
+
+} else{
+    console.log('Not Eligible for DL');
+}
+
+console.log(myvar);
+// console.log(x);
+
+console.log(1 + '1' + 5);
+console.log(5 + 1 + '1');
+console.log('1' + 5 + 1);
+console.log('1' + (5 + 1));
+console.log('1' == 1);
+console.log('1' === 1);
+console.log(4**3);
+console.log('ha'*5);
+console.log('ha'.repeat(5));
+console.log(1 + '1' - 1);
---
+++ Section 1/javascript/index.js
@@ -0,0 +1,15 @@
+console.log('Hello JavaScript');
+
+num = 891;
+
+console.log(num);
+
+my_name = 'Aditya';
+
+console.log(my_name);
+
+ans = true;
+
+console.log(ans);
+
+console.log(typeof ans);
