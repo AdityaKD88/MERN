1ffa1bf499a8ab79f0d522904098adb7edbe67fa
AdityaKD88 <90971914+AdityaKD88@users.noreply.github.com>
2023-10-20 15:24:55 +0530

    connecting db

---
+++ Section 3/backend/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://Aditya:akd889977@cluster0.yjttxm4.mongodb.net/mydatabase?retryWrites=true&w=majority";
+
+mongoose.connect(url)
+
+.then((result) => {
+    console.log('database connected');
+    // console.log(result);
+})
+.catch((err) => {
+    console.log(err);
+});
