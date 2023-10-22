d67d92e2f3935ecfe83ecdbaa08c3646091371d2
AdityaKD88 <90971914+AdityaKD88@users.noreply.github.com>
2023-10-22 19:58:45 +0530

    model

--- Section 3/backend/routers/userRouter.js
+++ Section 3/backend/routers/userRouter.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const Model = require('../models/userModel');
 
 const router = express.Router();
 
 router.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('add response from user');
+
+    new Model(req.body).save()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+    // res.send('add response from user');
 });
 
 router.get('/getall', (req, res) => {
